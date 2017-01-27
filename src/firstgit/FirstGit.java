44320633c5dd46fd0d972091dea1d89f87e3e739
agnas <agnas@L11704042767.wctc.msft>
2017-01-26 20:55:57 -0600

    Added a message in main

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -7,6 +7,9 @@ public class FirstGit {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello World");
+        
     }
     
 }
